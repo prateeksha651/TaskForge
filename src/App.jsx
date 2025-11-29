8db845f265e73444d314e143e55aa02c4755a65d
prateeksha651 <hsp86258@gmail.com>
2025-11-29 19:00:08 +0530

    Task Forge code

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { useState,useEffect } from 'react';
+import Taskform from './Components/Taskform'
+import Tasklist from './Components/Tasklist'
+import Progresstracker from './Components/Progresstracker'
+import './Components/Style.css'
+
+
+export default function App() {
+  const [tasks, setTasks] = useState([]);
+
+
+  useEffect(()=>{
+    localStorage.setItem("tasks",JSON.stringify(tasks));
+  });
+
+
+  const addTask = (task)=>{
+    setTasks([...tasks, task])
+  }
+
+
+  const updateTask = (updatedTask, index) => {
+    const newtask = [...tasks];
+    newtask[index] = updatedTask;
+    setTasks(newtask);
+  }
+  const deleteTask = (index) => {
+    setTasks(tasks.filter((_, i) => i != index ))
+  }
+
+
+  const clearTasks = () => {
+    setTasks([]);
+  }
+  return (
+    <div>
+      <header>
+        <h1>TaskForge</h1>
+        <p><i>Create. Organize. Complete.</i></p>
+      </header>
+
+
+      <Taskform addTask = {addTask}/>
+      <Tasklist tasks = {tasks}
+       updateTask = {updateTask} 
+       deleteTask = {deleteTask}/>
+      <Progresstracker tasks = {tasks}/>
+
+
+      {tasks.length>0 && (<button className='clear-btn'
+      onClick={clearTasks}>clear All Tasks</button>)}
+      
+    </div>
+  )
+}
